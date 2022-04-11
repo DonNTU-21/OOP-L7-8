7e635ceb343d2be679890f5206148f8f9821b177
tehrelt <haxrelt@gmail.com>
2022-04-11 10:35:45 +0300

    Добавьте файлы проекта.

---
+++ OOP-L7-8/Source.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <math.h>
+#include "CList.h"
+
+using namespace std;
+
+//template <class T>
+//class CList
+//{
+//	struct node
+//	{
+//		T* data;
+//		node* next;
+//	};
+//	node* head,
+//		* tail,
+//		* p;
+//	void Clear(void)
+//	{
+//		node* c = head;
+//		while (head)
+//		{
+//			c = head;
+//			head = head->next;
+//			delete c;
+//		}
+//	}
+//
+//public:
+//	CList() { head = tail = p = 0; }
+//	~CList() { Clear(); }
+//
+//	void Add(T* data)
+//	{
+//		if (head)
+//		{
+//			p->next = (node*)malloc(sizeof(node));
+//			p = p->next;
+//		}
+//		else
+//		{
+//			head = (node*)malloc(sizeof(node));
+//			p = head;
+//			tail = head;
+//		}
+//		p->data = data;
+//		p->next = 0;
+//	}
+//	bool Remove(T* data)
+//	{
+//		node* c = head;
+//		node* p1;
+//		if (*(head->data) == *data)
+//		{
+//			p1 = head;
+//			head = head->next;
+//			delete p1;
+//			return true;
+//		}
+//
+//		while (c->next && *(c->next->data) != *data)
+//		{
+//			c = c->next;
+//		}
+//		if (c->next)
+//		{
+//			p1 = c->next;
+//			c->next = c->next->next;
+//			delete p1;
+//			return true;
+//		}
+//		return false;
+//	}
+//	void Display()
+//	{
+//		node* c = head;
+//		while (c)
+//		{
+//			cout << *(c->data) << " ";
+//			c = c->next;
+//		}
+//		cout << endl;
+//	}
+//	bool DuplicateN(int n)
+//	{
+//		node* c = head;
+//		node* w;
+//		T* temp;
+//		bool a = false;
+//		int i=1;
+//		while (c)
+//		{
+//			if (i == n)
+//			{
+//				w = new node();
+//				w->data = c->data;
+//				w->next = c->next;
+//				c->next = w;			
+//				c = c->next->next;
+//				i = 0;
+//			}
+//			else
+//			{
+//				c = c->next;
+//			}
+//			i++;
+//		}
+//		return a;
+//	}
+//};
+
+
+int main()
+{
+	CList<int> ints;
+	CList<double> doubles;
+	for (int i = 0; i < 10; i++)
+	{
+		ints.Add(new int(i+1));
+		doubles.Add(new double(cosh(i)));
+	}
+	ints.Display();
+	ints.DuplicateN(2);
+	//ints.DuplicateN(1);
+	ints.Display();
+}
